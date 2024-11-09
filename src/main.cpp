b79606872e4c73407e44bd3d0b35a2a890f48a9c
Martin Mušinka <martinmusinka@MuskyMac.local>
2024-11-09 16:08:01 +0100

    Initial commit for SmartPot Project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define LED_PIN 2  // Most ESP32 boards have an onboard LED connected to GPIO 2
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_PIN, HIGH);  // Turn the LED on
+    delay(1000);                  // Wait for 1 second
+    digitalWrite(LED_PIN, LOW);   // Turn the LED off
+    delay(200);                  // Wait for 1 second
+}
